52ebaee650f56c5c7ed8f8be67858eee217ee8ad
Michele <Michele@air-di-michele.home-life.hub>
2021-03-09 10:54:54 +0100

    Classe Parole

--- Lab01_Parole/src/main/java/it/polito/tdp/parole/model/Parole.java
+++ Lab01_Parole/src/main/java/it/polito/tdp/parole/model/Parole.java
@@ -1,24 +1,39 @@
 package it.polito.tdp.parole.model;
 
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Parole {
+	
+	List <String> parole;
 		
 	public Parole() {
-		//TODO
+		parole = new LinkedList<>();
 	}
 	
 	public void addParola(String p) {
-		//TODO
+		parole.add(p);
+	}
+	
+	public class ComparatoreParole implements Comparator<String>{
+
+		@Override
+		public int compare(String p1, String p2) {
+			return p1.compareTo(p2);
+		}
+		
 	}
 	
 	public List<String> getElenco() {
-		//TODO
-		return null;
+		List <String> pp = new LinkedList<>(parole);
+		Collections.sort(pp, new  ComparatoreParole());
+		return pp;
 	}
 	
 	public void reset() {
-		// TODO
+		parole.clear();
 	}
 
 }
